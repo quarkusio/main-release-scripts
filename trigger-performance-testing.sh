8cc748db9a48fbec4efdd55d1da2c6dcdfe51185
John OHara <johara@redhat.com>
2023-03-29 05:48:25 +0100

    update perf lab ci URL

--- trigger-performance-testing.sh
+++ trigger-performance-testing.sh
@@ -13,4 +13,4 @@ else
   VERSION=$1
 fi 
 
-curl -k "http://mwperf-server01.perf.lab.eng.rdu2.redhat.com:8080/job/Products/job/Quarkus/job/regression/job/quarkus-regression-pipeline/buildWithParameters?token=quarkus-release&VERSION=${VERSION}"
+curl -k "https://ci.app-svc-perf.corp.redhat.com/job/Products/job/Quarkus/job/regression/job/quarkus-regression-pipeline/buildWithParameters?token=quarkus-release&VERSION=${VERSION}"
